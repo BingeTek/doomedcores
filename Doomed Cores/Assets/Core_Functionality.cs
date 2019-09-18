9ebb3234e7a89a71e175fbda9b6f84e257607de4
GuardianT90 <GuardianT90@users.noreply.github.com>
2019-09-18 17:59:02 -0500

    Basic Core
    Just a core and a basic unfinished script file

---
+++ Doomed Cores/Assets/Core_Functionality.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core_Functionality : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int coreLife = 10;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
