eb64adcdcc3f6fdad1a2d63ed3488eb6e91644bc
GuardianT90 <GuardianT90@users.noreply.github.com>
2019-09-18 19:13:50 -0500

    The core lives and dies
    Made it so that the core has health and when clicking on the core it takes damage. Once the health reaches 0 the core disappears.

--- Doomed Cores/Assets/Core_Functionality.cs
+++ Doomed Cores/Assets/Core_Functionality.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Core_Functionality : MonoBehaviour
 {
+    int coreLife;
+
     // Start is called before the first frame update
     void Start()
     {
-        int coreLife = 10;
-        
+        coreLife = 10;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if()
+        if (Input.GetKeyUp(KeyCode.Mouse0) && coreLife != 0)
+        {
+            print($"{coreLife}");
+            coreLife--;
+        }
+
+        if(coreLife == 0)
+        {
+            print("Core is dead YOU LOSE");
+            Destroy(gameObject);
+        }
+
     }
 }
